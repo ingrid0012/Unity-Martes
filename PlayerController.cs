8a00639518daa64b145a612f9611da0d091ed6ec
Marce Pinzon <marcelapinzon95@gmail.com>
2025-04-22 20:14:31 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,75 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI; //trabajar el canvas
 using Photon.Pun;
+using Photon.Realtime;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
 
-
-
-
-public class PlayerController : MonoBehaviourPunCallbacks
+public class PlayerController : MonoBehaviourPunCallbacks /*IPunObservable*/
 {
-    public enum Weapons //m3l4 
+    public enum Weapons
     {
         None,
         Pistol,
         Rifle,
         MiniGun
     }
-    Weapons weapons = Weapons.None; //m3l4
+    Weapons weapons = Weapons.None;    
     
-    [SerializeField] float movementSpeed = 5f;
-    [SerializeField] GameObject pistol, rifle, miniGun; //m3l4
-    bool isPistol, isRifle, isMiniGun; //m3l4
-    float currentSpeed;
-
-
-    
-
+    [SerializeField] GameObject pistol, rifle, miniGun;
+    [SerializeField] AudioSource characterSounds;
+    [SerializeField] AudioClip jump;
+    [SerializeField] Image pistolUI, rifleUI, miniGunUI, cusror;
     [SerializeField] Rigidbody rb;
-    Vector3 direction;
-
+    [SerializeField] Animator anim;
     [SerializeField] float shiftSpeed = 10f;
     [SerializeField] float jumpForce = 7f;
-    float stamina = 5f;
-
+    [SerializeField] float movementSpeed = 5f;
+    Vector3 direction;
+    bool isPistol, isRifle, isMiniGun;
     bool isGrounded = true;
-
-    [SerializeField] Animator anim;  
-    [SerializeField] Image pistolUI, rifleUI, miniGunUI, cursor;
+    float currentSpeed;
+    float stamina = 5f;
+    int health;
+    public bool dead;
+    TextUpdate textUpdate;
+    [SerializeField] GameObject damageUi;
+    GameManager gameManager;
     
-    private int health; // la salud de personaje
-
-    [SerializeField] AudioSource characterSounds; // Hacemos referencia al AudioSource
-    [SerializeField] AudioClip jump; // Hacemos referencia al clip de sonido del salto
-
-   
     void Start()
-    {        
+    {
+        gameManager = FindObjectOfType<GameManager>();   
+        gameManager.ChangePlayersList();
+
+        textUpdate = GetComponent<TextUpdate>();
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         currentSpeed = movementSpeed;
-        health=100; 
-       if (!photonView.IsMine)
-        {                       
-            transform.Find("Main Camera").gameObject.SetActive(false);
-            Destroy(GetComponent<PlayerController>());
+        health = 100;
+        if (!photonView.IsMine)
+        {            
+            transform.Find("Main Camera").gameObject.SetActive(false);           
+            transform.Find("Canvas").gameObject.SetActive(false);
+            this.enabled = false;
         }
     }
-
+    public void GetDamage(int count)
+    {
+        photonView.RPC("ChangeHealth", RpcTarget.All, count);
+    }
+    [PunRPC]
     public void ChangeHealth(int count)
     {
-        health -= count; //restando vida 
-        if (health<=0) // si la salud llega a cero algo pasará 
+        health -= count;
+        textUpdate.SetHealth(health);
+        damageUi.SetActive(true);
+        Invoke("RemoveDamageUi", 0.1f);
+        if (health <= 0) 
         {
-            anim.SetBool("Die", true); //animación
-            ChooseWeapon(Weapons.None); //el arma desaparece
-            this.enabled = false; // no nos vamos a poder mover 
+            dead = true;
+            anim.SetBool("Die", true);
+            transform.Find("Main Camera").GetComponent<ThirdPersonCamera>().isSpectator = true;
+            ChooseWeapon(Weapons.None);
+            gameManager.ChangePlayersList();
+            //gameManager.PlayerDied(gameObject.name);
+            this.enabled = false;
         }
+    }   
+    void RemoveDamageUi()
+    {
+        damageUi.SetActive(false);
     }
-
-
-
     
     void Update()
     {
@@ -78,30 +86,33 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
         direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
         direction = transform.TransformDirection(direction);
-        if(direction.x != 0 || direction.z != 0)
+
+        if (direction.x != 0 || direction.z != 0)
         {
             anim.SetBool("Run", true);
-            if(!characterSounds.isPlaying&& isGrounded)
+            if (!characterSounds.isPlaying && isGrounded)
             {
                 characterSounds.Play();
             }
         }
-        if(direction.x == 0 && direction.z == 0)
+        if (direction.x == 0 && direction.z == 0)
         {
             anim.SetBool("Run", false);
             characterSounds.Stop();
         }
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
             isGrounded = false;
-            characterSounds.Stop(); //se desactiva el sonido de correr
+            characterSounds.Stop();
             AudioSource.PlayClipAtPoint(jump, transform.position);
             anim.SetBool("Jump", true);
         }
-        if(Input.GetKey(KeyCode.LeftShift))
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            if(stamina > 0)
+            if (stamina > 0)
             {
                 stamina -= Time.deltaTime;
                 currentSpeed = shiftSpeed;
@@ -112,11 +123,11 @@ public class PlayerController : MonoBehaviourPunCallbacks
             }
         }
         else if (!Input.GetKey(KeyCode.LeftShift))
-        {            
-            stamina += Time.deltaTime;                      
+        {
+            stamina += Time.deltaTime;
             currentSpeed = movementSpeed;
         }
-        if(stamina > 5f)
+        if (stamina > 5f)
         {
             stamina = 5f;
         }
@@ -124,25 +135,25 @@ public class PlayerController : MonoBehaviourPunCallbacks
         {
             stamina = 0;
         }
-        if(Input.GetKeyDown(KeyCode.Alpha1) && isPistol) //m3l4
+        if (Input.GetKeyDown(KeyCode.Alpha1) && isPistol)
         {
-            ChooseWeapon(Weapons.Pistol);
+            photonView.RPC("ChooseWeapon", RpcTarget.All, Weapons.Pistol);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2) && isRifle)
         {
-            ChooseWeapon(Weapons.Rifle);
+            photonView.RPC("ChooseWeapon", RpcTarget.All, Weapons.Rifle);
         }
         if (Input.GetKeyDown(KeyCode.Alpha3) && isMiniGun)
         {
-            ChooseWeapon(Weapons.MiniGun);
+            photonView.RPC("ChooseWeapon", RpcTarget.All, Weapons.MiniGun);
         }
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            ChooseWeapon(Weapons.None);
+            photonView.RPC("ChooseWeapon", RpcTarget.All, Weapons.None);
         }
-
     }
-    public void ChooseWeapon(Weapons weapons) //m3l4
+    [PunRPC]
+    public void ChooseWeapon(Weapons weapons)
     {
         anim.SetBool("Pistol", weapons == Weapons.Pistol);
         anim.SetBool("Assault", weapons == Weapons.Rifle);
@@ -151,37 +162,34 @@ public class PlayerController : MonoBehaviourPunCallbacks
         pistol.SetActive(weapons == Weapons.Pistol);
         rifle.SetActive(weapons == Weapons.Rifle);
         miniGun.SetActive(weapons == Weapons.MiniGun);
-
-        if(weapons != Weapons.None) //activamos o desactivamos el cursor 
+        
+        if(weapons != Weapons.None)
         {
-            cursor.enabled=true;
+            cusror.enabled = true;
         }
         else
         {
-            cursor.enabled=false;
+            cusror.enabled = false;
         }
-
-        
     }
-    
     void FixedUpdate()
     {
         rb.MovePosition(transform.position + direction * currentSpeed * Time.deltaTime);
     }
     void OnCollisionEnter(Collision collision)
-    {
+    {        
         isGrounded = true;
         anim.SetBool("Jump", false);
     }
-    private void OnTriggerEnter(Collider other) //m3l4
+    private void OnTriggerEnter(Collider other)
     {
-        switch (other.gameObject.tag) 
+        switch (other.gameObject.tag)
         {
             case "pistol":
                 if (!isPistol)
                 {
                     isPistol = true;
-                    pistolUI.color=Color.green; //el arma cuando la tenga cambie a V.
+                    pistolUI.color = Color.white;
                     ChooseWeapon(Weapons.Pistol);
                 }
                 break;
@@ -190,7 +198,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
                 if (!isRifle)
                 {
                     isRifle = true;
-                    rifleUI.color=Color.green;
+                    rifleUI.color = Color.white;
                     ChooseWeapon(Weapons.Rifle);
                 }
                 break;
@@ -199,7 +207,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
                 if (!isMiniGun)
                 {
                     isMiniGun = true;
-                    miniGunUI.color=Color.green;
+                    miniGunUI.color = Color.white;
                     ChooseWeapon(Weapons.MiniGun);
                 }
                 break;
@@ -207,8 +215,7 @@ public class PlayerController : MonoBehaviourPunCallbacks
                 break;
         }
         Destroy(other.gameObject);
-    }   
-
-
-
+    }
+    
+   
 }
